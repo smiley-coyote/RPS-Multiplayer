430ab086c854f27668c469e1f53aeef1cbeb78a1
smiley-coyote <nick.a.alex@gmail.com>
2018-06-24 16:15:50 -0400

    files all set up and ready to code

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB4GYlrPMMf_gFIyZLmYOY-y4SIBR8IHmk",
+    authDomain: "rps-multiplayer-5e7ef.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-5e7ef.firebaseio.com",
+    projectId: "rps-multiplayer-5e7ef",
+    storageBucket: "",
+    messagingSenderId: "754178871562"
+  };
+  firebase.initializeApp(config);
