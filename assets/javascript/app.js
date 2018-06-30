14cc8a0fdef50dcf67b4cda56d7d77fac989841f
smiley-coyote <nick.a.alex@gmail.com>
2018-06-30 18:33:24 -0400

    functionality works. need to add design and wins losses

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,127 +1,314 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyB4GYlrPMMf_gFIyZLmYOY-y4SIBR8IHmk",
-    authDomain: "rps-multiplayer-5e7ef.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-5e7ef.firebaseio.com",
-    projectId: "rps-multiplayer-5e7ef",
-    storageBucket: "",
-    messagingSenderId: "754178871562"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB4GYlrPMMf_gFIyZLmYOY-y4SIBR8IHmk",
+  authDomain: "rps-multiplayer-5e7ef.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-5e7ef.firebaseio.com",
+  projectId: "rps-multiplayer-5e7ef",
+  storageBucket: "rps-multiplayer-5e7ef.appspot.com",
+  messagingSenderId: "754178871562"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
-  var rps = ["rock", "paper", "scissors"];
-  var player1 = "";
-  var player2 = "";
-  var player1Select = false;
-  var database = firebase.database();
+var curUser;
+var rps = ["rock", "paper", "scissors"];
+var userNum = Math.floor(Math.random() * 100000);
+var playerSelect = false;
 
-  $(document).on("click", "#player-submit", function() {
-      event.preventDefault();
-      if (!player1Select) {
-        player1 = $("#player-input").val().trim();
-        $("#player-input").val("");
-        player1Select = true;
-        database.ref().push({
-            player1: player1
-        })
-        $("#player-1").html("<h2>Player 1: " + player1 + "</h2>");
-        $("#player").html(" <h2>Player 2:</h2> <h2>Enter your name</h2>")
-      } else {
-          player2 = $("#player-input").val().trim();
-          console.log(player2);
-          $("#player-input").val("");
-          database.ref().push({
-            player2: player2
-        })
-        $("#player-2").html("<h2>Player 2: " + player2 + "</h2>");
-        $("#game-select").html("");
-      }
+var value;
+var player1Name;
+var player2Name;
+var gameStart = false;
+var player1Choice = "";
+var player2Choice = "";
+
+
+database.ref().on("value", function(snapshot) {
+  
+  var seats = parseInt(snapshot.val().seats);
+  
+  console.log(seats);
+  
+  
+  
+  if(curUser !== undefined && seats == 2 && !playerSelect){
+    playerSelect = true;
+    $("#player1-ID").html("<h1>" + curUser + "</h1>")
+    $("#player2-ID").html("<h1>Waiting for player 2 to start...</h1>")
+    player1();
+   database.ref().update({
+     seats: 1
+   })
+   database.ref("player1/").update({
+     name: curUser
+   })
+   $("#game-start").html("");
+   
+  } else if (curUser !== undefined && seats == 1 && !playerSelect){
+    playerSelect = true;
+    player1Name = snapshot.val().player1.name;
+    
+    $("#player1-box").html("<h2>" + player1Name + " is ready to play</h2>")
+    $("#player2-ID").html("<h1>" + curUser + "</h1>")
+    player2();
+    database.ref().update({
+      seats: 0
+    })
+    database.ref("player2/").update({
+      name: curUser
+    })
+    $("#game-start").html("");
+  } else if (!playerSelect && seats == 0){
+    playerSelect = true;
+    alert("game is full");
+    
+  } else if (playerSelect && seats == 0){
+    player2Name = snapshot.val().player2.name;
+    player1Name = snapshot.val().player1.name;
+    $("#player2-ID").html("<h1>" + player2Name + "</h1>")
+
+  }
+
+})
+
+$(document).on("click", "#player-submit", function () {
+  event.preventDefault();
+  curUser = $("#player-name").val().trim();
+  $("#player-input").val("");
+ 
+  database.ref("user/").push({
+    player: userNum
   })
 
-  /* firebase multiplayer code
-  
-  function go() {
-  var userId = prompt('Username?', 'Guest');
-  // Consider adding '/<unique id>' if you have multiple games.
-  var gameRef = new Firebase(GAME_LOCATION);
-  assignPlayerNumberAndPlayGame(userId, gameRef);
-};
+});
 
-// The maximum number of players.  If there are already 
-// NUM_PLAYERS assigned, users won't be able to join the game.
-var NUM_PLAYERS = 4;
-
-// The root of your game data.
-var GAME_LOCATION = 'https://SampleGame.firebaseIO-demo.com/';
-
-// A location under GAME_LOCATION that will store the list of 
-// players who have joined the game (up to MAX_PLAYERS).
-var PLAYERS_LOCATION = 'player_list';
-
-// A location under GAME_LOCATION that you will use to store data 
-// for each player (their game state, etc.)
-var PLAYER_DATA_LOCATION = 'player_data';
-
-
-// Called after player assignment completes.
-function playGame(myPlayerNumber, userId, justJoinedGame, gameRef) {
-  var playerDataRef = gameRef.child(PLAYER_DATA_LOCATION).child(myPlayerNumber);
-  alert('You are player number ' + myPlayerNumber + 
-      '.  Your data will be located at ' + playerDataRef.toString());
-
-  if (justJoinedGame) {
-    alert('Doing first-time initialization of data.');
-    playerDataRef.set({userId: userId, state: 'game state'});
+function player1() {
+  for (i = 0; i < rps.length; i++) {
+    var newButton = $("<button>" + rps[i] + "</button>");
+    newButton.addClass("play-button");
+    newButton.attr("id", rps[i])
+    newButton.val(rps[i]);
+    $("#player1-buttons").append(newButton);
+  }
+}
+function player2() {
+  for (i = 0; i < rps.length; i++) {
+    var newButton = $("<button>" + rps[i] + "</button>");
+    newButton.addClass("play-button");
+    newButton.attr("id", rps[i])
+    newButton.val(rps[i]);
+    $("#player2-buttons").append(newButton);
   }
 }
 
-// Use transaction() to assign a player number, then call playGame().
-function assignPlayerNumberAndPlayGame(userId, gameRef) {
-  var playerListRef = gameRef.child(PLAYERS_LOCATION);
-  var myPlayerNumber, alreadyInGame = false;
+$(document).on("click", ".play-button", function(){
+  value = $(this).val();
 
-  playerListRef.transaction(function(playerList) {
-    // Attempt to (re)join the given game. Notes:
-    //
-    // 1. Upon very first call, playerList will likely appear null (even if the
-    // list isn't empty), since Firebase runs the update function optimistically
-    // before it receives any data.
-    // 2. The list is assumed not to have any gaps (once a player joins, they 
-    // don't leave).
-    // 3. Our update function sets some external variables but doesn't act on
-    // them until the completion callback, since the update function may be
-    // called multiple times with different data.
-    if (playerList === null) {
-      playerList = [];
+  if (curUser == player1Name){
+    if (value == "rock"){
+      player1Choice = "rock";
+      database.ref("playerChoice/").update({
+        one: player1Choice
+      })
+      return;
+    }
+    if (value == "paper"){
+      player1Choice = "paper";
+      database.ref("playerChoice/").update({
+        one: player1Choice
+      })
+      return;
+    }
+    if (value == "scissors"){
+      player1Choice = "scissors";
+      database.ref("playerChoice/").update({
+        one: player1Choice
+      })
+      return;
+    }
+  }
+  if (curUser == player2Name){
+    if (value == "rock"){
+      player2Choice = "rock";
+      database.ref("playerChoice/").update({
+        two: player2Choice
+      })
+      return;
+    }
+    if (value == "paper"){
+      player2Choice = "paper";
+      database.ref("playerChoice/").update({
+        two: player2Choice
+      })
+      return;
+    }
+    if (value == "scissors"){
+      player2Choice = "scissors";
+      database.ref("playerChoice/").update({
+        two: player2Choice
+      })
+      return;
+    }
+  }
+})
+
+database.ref("playerChoice/").on("value", function(snapshot){
+  player1Choice = snapshot.val().one;
+  player2Choice = snapshot.val().two;
+  if (player1Choice !== "" && player2Choice !== ""){
+    
+    if (player1Choice == "rock" && player2Choice == "paper"){
+      playerOneLose();
+    }
+    if (player1Choice == "rock" && player2Choice == "scissors"){
+      playerOneWin();
+    }
+    if (player1Choice == "paper" && player2Choice == "rock"){
+      playerOneWin();
+    }
+     if (player1Choice == "paper" && player2Choice == "scissors"){
+      playerOneLose();
+    }
+    if (player1Choice == "scissors" && player2Choice == "rock"){
+      playerOneLose();
+    }
+    if (player1Choice == "scissors" && player2Choice == "paper"){
+      playerOneWin();
+    }
+    if (player1Choice == "scissors" && player2Choice == "scissors"){
+      playerTie();
+    }
+    if (player1Choice == "paper" && player2Choice == "paper"){
+      playerTie();
+    }
+    if (player1Choice == "rock" && player2Choice == "rock"){
+      playerTie();
     }
 
-    for (var i = 0; i < playerList.length; i++) {
-      if (playerList[i] === userId) {
-        // Already seated so abort transaction to not unnecessarily update playerList.
-        alreadyInGame = true;
-        myPlayerNumber = i; // Tell completion callback which seat we have.
-        return;
-      }
-    }
+    database.ref("playerChoice/").update({
+      one: "",
+      two: ""
+    })
+  }
+ 
+})
 
-    if (i < NUM_PLAYERS) {
-      // Empty seat is available so grab it and attempt to commit modified playerList.
-      playerList[i] = userId;  // Reserve our seat.
-      myPlayerNumber = i; // Tell completion callback which seat we reserved.
-      return playerList;
-    }
 
-    // Abort transaction and tell completion callback we failed to join.
-    myPlayerNumber = null;
-  }, function (error, committed) {
-    // Transaction has completed.  Check if it succeeded or we were already in
-    // the game and so it was aborted.
-    if (committed || alreadyInGame) {
-      playGame(myPlayerNumber, userId, !alreadyInGame, gameRef);
-    } else {
-      alert('Game is full.  Can\'t join. :-(');
-    }
-  });
+
+function playerTie(){
+  console.log("ITS A TIE")
 }
+function playerOneWin(){
+  console.log("PLAYER ONE WINS")
+}
+function playerOneLose(){
+  console.log("PLAYER ONE LOSES")
+}
+
+
+
+
+
+/*
+var counter = 1;
+
+$("#send").click(function () {
+    var time = new Date().toLocaleTimeString();
+    var message = $("#message-input").val();
+    $("#message-input").val('');
+    chatroom.push({
+        username: curUser,
+        message: message,
+        time: time
+    })
+
+    return false;
+
+});
+
+var chatroom = firebase.database().ref("theHackersChatroom");
+
+chatroom.on('value', function(snapshot){
+    updateUI(snapshot.val());
+})
+
+
+
 */
 
+
+
+
+
+
+/* CHAT===========================================================================================================================
+//use newMessage to add a new message to the UI
+// isCurUser should be set to true or false
+function newMessage(message, username, time) {
+  var position;
+  if (curUser == username) {
+    position = 'left'
+  }
+  else {
+    position = 'right'
+  }
+
+  $('.messages > ul').append($("<li class='li-" + position + "'><span class='li-message'>" + message + "</span><span class='li-username'>- " + username + " | " + time + "</span></li>"));
+  $(".messages").animate({ scrollTop: $(this)[0].scrollHeight }, 1000);
+}
+
+function scrollToBottom() {
+  $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight }, 1000);
+}
+
+
+function updateUI(messages) {
+  $('.messages > ul').html('');
+
+
+  for (var key in messages) {
+    var message = messages[key];
+    var isCurUser = curUser == message.username;
+
+    newMessage(messages[key].message, messages[key].username, messages[key].time);
+  }
+
+  scrollToBottom();
+}
+
+
+*/
+
+
+
+
+// My original code
+
+// var rps = ["rock", "paper", "scissors"];
+// var player1 = "";
+// var player2 = "";
+// var player1Select = false;
+// var database = firebase.database();
+
+// $(document).on("click", "#player-submit", function () {
+//     event.preventDefault();
+//     if (!player1Select) {
+//         player1 = $("#player-input").val().trim();
+//         $("#player-input").val("");
+//         player1Select = true;
+//         database.ref().push({
+//             player1: player1
+//         })
+//         $("#player-1").html("<h2>Player 1: " + player1 + "</h2>");
+//         $("#player").html(" <h2>Player 2:</h2> <h2>Enter your name</h2>")
+//     } else {
+//         player2 = $("#player-input").val().trim();
+//         console.log(player2);
+//         $("#player-input").val("");
+//         database.ref().push({
+//             player2: player2
+//         })
+//         $("#player-2").html("<h2>Player 2: " + player2 + "</h2>");
+//         $("#game-select").html("");
+//     }
+// })
